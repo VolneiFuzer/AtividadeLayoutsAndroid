04402d9a10cfac6962ea8bec0b293d720206c94c
Volnei Fuzer <volnei.fuzer@gmail.com>
2016-09-10 22:31:38 -0300

    Atividade da cadeira de programação para dispositivos portáteis

---
+++ AtividadeLayouts/app/src/main/java/br/cesed/atividadelayouts/GridLayoutActivity.java
@@ -0,0 +1,13 @@
+package br.cesed.atividadelayouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GridLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_layout);
+    }
+}
